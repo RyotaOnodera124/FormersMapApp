4d83385f1708afc45eb799768d564bc80b74bd93
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-13 10:09:08 +0900

    Initial Commit

---
+++ FormersMapApp/FormersMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormersMapAppApp.swift
+//  FormersMapApp
+//
+//  Created by 小野寺良太 on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct FormersMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
